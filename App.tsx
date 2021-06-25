5456400f7e53213bcb49d6e2c4700123a55bb53b
jhoncsr <bsi.jcsouza@gmail.com>
2021-06-25 00:27:28 -0300

    Rota tela login para home

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ import {Inter_400Regular,Inter_500Medium } from '@expo-google-fonts/inter'
 import {Rajdhani_500Medium,Rajdhani_700Bold } from '@expo-google-fonts/rajdhani'
 import AppLoading from 'expo-app-loading'
 
-import {Home} from './src/screens/Home/';
+import {Routes} from './src/routes';
 import { Background } from './src/components/Background';
 
 export default function App() {
@@ -19,12 +19,13 @@ export default function App() {
     return <AppLoading/>
   }
   return (
-    <Background>
+    <>
+   
     <StatusBar
     barStyle="light-content"
     backgroundColor="transparent"
     translucent/>
-   <Home/>
-    </Background>
+   <Routes/>
+    </>
   );
 }
--- src/screens/Signin/index.tsx
+++ src/screens/Signin/index.tsx
@@ -1,22 +1,33 @@
 import React, { useState } from 'react';
-import { 
+import {
     Text,
     View,
     Image,
 } from 'react-native';
 import IllustrationImg from '../../assets/SignIn/illustration.png';
-import {styles} from './styles';
-import {ButtonIcon} from '../../components/ButtonIcon'
+import { styles } from './styles';
+import { ButtonIcon } from '../../components/ButtonIcon'
+
+import { useNavigation } from '@react-navigation/native';
+
+
+
 
 export function SignIn() {
+    const navigation = useNavigation();
+
+    function handleSignIn() {
+        navigation.navigate('Home');
+    }
+
     const [text, setText] = useState('');
-        return (
-            <View style= {styles.container}>
-                <Image 
+    return (
+        <View style={styles.container}>
+            <Image
                 source={IllustrationImg}
-                style = {styles.image}
-                />
-            <View style = {styles.content}>
+                style={styles.image}
+            />
+            <View style={styles.content}>
                 <Text style={styles.title}>
                     Conecte-se {`\n`}
                     e organize suas {`\n`}
@@ -25,13 +36,14 @@ export function SignIn() {
                 <Text style={styles.subtitle}>
                     Crie grupos para jogar com seus games {`\n`}
                     favoritos com seus amigos
-                   
+
                 </Text>
                 <ButtonIcon
-                title= "Entrar com Discord"
-                activeOpacity= {0.7}/>
+                    title="Entrar com Discord"
+                    activeOpacity={0.7} 
+                    onPress={handleSignIn}/>
             </View>
-            </View>
+        </View>
 
-        )
+    )
 }
